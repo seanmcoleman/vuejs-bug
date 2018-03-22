cbb3c2cd8ee29f941ce91dc76eca0fec8fab4a28
Sean Coleman <sean@seancoleman.net>
2018-03-21 19:01:20 -0700

    Bu

---
+++ src/components/ComponentWithSymbolProp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+   component
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentWithSymbolProp',
+  props: {
+    symbolProp: {
+      type: Symbol,
+      required: true,
+    },
+  },
+};
+</script>
